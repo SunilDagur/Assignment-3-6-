967e0c9fb5d246f82849ff288a0dfb84cb91a3ff
Sunil Dagur <sunildagur566@gmail.com>
2023-09-06 22:16:30 +0530

    Add project files.

---
+++ Assignment 3(6)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class MyBaseClass
+{
+    public string BaseProperty { get; set; }
+}
+class MyDerivedClass : MyBaseClass
+{
+    public void AccessBaseProperty()
+    {
+        BaseProperty = "Accessed from the derived class";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyDerivedClass derivedObject = new MyDerivedClass();
+
+        derivedObject.AccessBaseProperty();
+
+        Console.WriteLine("Property value from the derived class: " + derivedObject.BaseProperty);
+    }
+}
